aaa33ad9e621d17300899008a5b684fb34df0c69
夜月琉璃 <542112943@qq.com>
2022-08-23 16:41:14 +0800

    SearchCenter First Release
    SearchCenter First Release

---
+++ search.php
@@ -0,0 +1,112 @@
+<?php 
+// 搜索核心驱动模块	
+//error_reporting(0);
+require("kernl/Init.php"); //初始化基础参数
+ob_clean();
+$dou -> CheckServerState();//网站状态检查
+$searchdata = empty($_POST['searchdata']) ? '' : $_POST['searchdata'];  // 搜索内容
+$type = empty($_POST['searchfs']) ? '' : $_POST['searchfs'];  // 搜索方式
+
+switch($type)
+{
+	case 'mac':
+		//MAC地址分析
+		$pattern = "/^([A-Fa-f0-9]{2}[-,:]){5}[A-Fa-f0-9]{2}$/"  ;
+		if(!preg_match($pattern, $searchdata))
+		{
+			echo json_encode(array('status'=>'-1','message'=>'不是一个有效的MAC地址！<br> 有效的地址格式为：xx-xx-xx-xx-xx-xx'));
+			exit;
+		}
+		if(strpos($searchdata,"-") != FALSE) 
+		{
+			$replace1 = str_replace("-","",$searchdata);
+		} else {
+			if(strpos($searchdata,":") != FALSE) 
+			{
+				$replace1 = str_replace(":","",$searchdata);
+			}	
+		}
+		$querydata = substr($replace1,0,6);
+		$sql = 'select * from zd_mac_addr where OUI_HEX LIKE "%'.$querydata.'%"';
+		$row = $dou -> fetch_array($dou -> query($sql));
+		$count = $dou -> affected_rows();
+		switch($count)
+		{
+			case 0:
+			echo json_encode(array('status'=>'-1','message'=>'查询无果','test'=>$querydata));
+			exit;
+			
+			case 1:
+			echo json_encode(array('status'=>'0','message'=>'Success','mac'=>$searchdata,'mac_q'=>$querydata,'regcompy'=>$row['Organization'])); 
+			exit;
+		
+			default:
+			echo json_encode(array('status'=>'-1','message'=>'查询无果','test'=>$querydata));
+			exit;
+		}
+		break;
+		
+	case 'ip':
+		if($_POST['software_alive'] == 'get')
+		{
+			if($searchdata="local")
+			{
+				$demo = new IPQueryEngine('kernl/ipdata.dat'); // 初始化类
+				$detail = $demo->getDetail($demo->ip()); // 调用查询函数
+				$UserIP = $demo->ip(); // 调用查询函数
+				echo json_encode(array('status'=>'1','message'=>'Success','UserIP'=>$UserIP,'Data'=>$detail ));// 输出查询结果
+			
+			}
+		} else {
+			//IP地址分析
+			$pattern = '/((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}/'  ;
+			if(!preg_match($pattern, $searchdata))
+			{
+				echo json_encode(array('status'=>'-1','message'=>'不是一个有效的ip地址！'));
+				exit;
+			}
+			$demo = new IPQueryEngine('kernl/ipdata.dat'); // 初始化类
+			$detail = $demo->getDetail($searchdata); // 调用查询函数
+			$UserIP = $demo->ip(); // 调用查询函数
+			echo json_encode(array('status'=>'1','message'=>'Success','UserIP'=>$UserIP,'Data'=>$detail ));// 输出查询结果
+		}
+		break;
+		
+	case 'process':
+		$sql = 'select * from zd_process_list where p_name LIKE "%'.$searchdata.'%"';
+		$row = $dou -> fetch_array($dou -> query($sql));
+		$count = $dou -> affected_rows();
+		switch($count)
+		{
+			case 0:
+				echo json_encode(array('status'=>'-1','message'=>'查询无果'));
+				exit;
+				
+			case 1:
+				echo json_encode(array('status'=>'2','message'=>'Success','p_name'=>$searchdata,'p_path'=>$row['path'],'regcompy'=>$row['compy'],'p_level'=>$row['safevalue'],'p_other'=>$row['other'])); 
+				exit;
+		
+			default:
+				echo json_encode(array('status'=>'3','message'=>'Success','Data'=>$count));
+				exit;
+		}
+		break;
+		
+	case 'hz':
+		$sql = 'select * from zd_filetypetable where filetype LIKE "%'.$searchdata.'%"';
+		//print_r($sql);
+		$result = $dou->query($sql); 
+		$Data = array();
+		while ($row =$dou->fetch_array($result))
+		{		
+			$count=count($row);//不能在循环语句中，由于每次删除 row数组长度都减小  
+			for($i=0;$i<$count;$i++)
+			{  
+				unset($row[$i]);//删除冗余数据  
+			}
+			array_push($Data,$row);
+		}
+		echo json_encode(array('status'=>'4','message'=>'Success','count'=>$dou->num_rows($result),'Data'=>$Data));
+		break;
+}
+?>
---
+++ template/about.php
@@ -0,0 +1,34 @@
+<?php 
+require("../kernl/Init.php"); //初始化基础参数
+?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="renderer" content="webkit">
+	<link rel="stylesheet" href="../css/style.min.css">
+	<link rel="stylesheet" href="../fonts/material-design/css/materialdesignicons.css">
+	<link rel="stylesheet" href="../plugin/mCustomScrollbar/jquery.mCustomScrollbar.min.css">
+	<link rel="stylesheet" href="../plugin/waves/waves.min.css">
+	<link rel="stylesheet" href="../plugin/sweet-alert/sweetalert.css">
+	<link rel="stylesheet" href="../css/style-dark.min.css">
+</head>
+<body>
+<div class="row">
+	<div class="col-lg-4 col-md-6 col-xs-12">
+    	<div class="box-content">
+              <table class="table">
+                    <tr><td width="110" align="right">开发：</td><td>Source</td><td align="right">官网：</td><td><a href="http://bbs.csource.com.cn" target="_blank">点击访问</a></td></tr>
+                    <tr><td align="right">可查询数：</td><td>Mac Address：<?php echo $dou -> GetSearchCount('zd_mac_addr');  ?><br>Process：<?php echo $dou -> GetSearchCount('zd_process_list');  ?><br>FileType：<?php echo $dou -> GetSearchCount('zd_filetypetable');  ?></td><td align="right">更新时间：</td><td>2020-01-23</td></tr>
+                    <tr><td align="right">IP库版本：</td><td><?php echo $dou -> Get_IPVersion(); ?></td><td align="right">更新时间：</td><td><?php echo $dou -> Get_IPVersion(); ?></td></tr>					
+					<tr><td align="right">系统版本：</td><td>V1.0</td><td align="right">错误提交：</td><td><a href="http://bbs.csource.com.cn" target="_blank">点击提交</a></td></tr>
+					<tr><td align="right">源码下载：</td><td><a href="http://tool.csource.com.cn/QueryMAC.zip" target="_blank">点击下载</a>(<a href="https://github.com/mike4678/MacQuery" target="_blank">GitHub</a>)</td><td align="right">客户端下载：</td><td><a href="http://tool.csource.com.cn/QueryClient.zip" target="_blank">点击下载</a></td></tr>
+                </table>
+            </div>
+        </div>
+		            </div>
+    </div>    
+</div>
+</body></html>
